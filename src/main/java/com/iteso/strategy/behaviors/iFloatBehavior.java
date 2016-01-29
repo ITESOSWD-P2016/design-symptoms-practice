b6f29dcd594357b031b7786c640b6ea9d9665bf1
Rodrigo Villalobos <rodrigov@iteso.mx>
2016-01-29 07:03:59 -0600

    First commit

--- src/main/java/com/iteso/strategy/behaviors/iFloatBehavior.java
+++
@@ -1,9 +0,0 @@
-package com.iteso.strategy.behaviors;
-
-/**
- * Created by Paloma on 22/09/2015.
- */
-public interface iFloatBehavior {
-    String floating();
-
-}
--- src/main/java/com/iteso/strategy3/behaviors/iDefendingBehavior.java
+++
@@ -1,8 +0,0 @@
-package com.iteso.strategy3.behaviors;
-
-/**
- * Created by Paloma on 22/09/2015.
- */
-public interface iDefendingBehavior {
-    String Defend();
-}
