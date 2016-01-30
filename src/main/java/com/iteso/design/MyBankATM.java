61da769aa0f826827f7aae9a06b71cf570c90de4
CrissFranco <leafreo@gmail.com>
2016-01-30 00:01:54 -0600

    Checkpoint 2 corregido

--- src/main/java/com/iteso/design/MyBankATM.java
+++ src/main/java/com/iteso/design/MyBankATM.java
@@ -13,99 +13,51 @@ import java.util.Scanner;
  * To change this template use File | Settings | File Templates.
  */
 public class MyBankATM {
-    MyBankCard myCard = null;
+    String Number=null;
+    String Pin=null;
+    String Type=null;
 
     public MyBankATM() {
+
     }
     public static void main(String args[]){
-        MyBankATM myAtm = new MyBankATM();
+        MyBankCard myCard = new MyBankCard();
+        MyBankATM myATM = new MyBankATM();
 
-
+        boolean valid=false;
         //ATM Should Request Card
-        try {
-            myAtm.askForCard();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            myATM.Number = myCard.askForCard();
+
+
+
+        if (myATM.Number.startsWith("1")) {
+            myATM.Type = "1";
+            PIN_4digits myPin = new PIN_4digits();
+            myATM.Pin = myPin.AskForPin();
+            valid = myPin.ValidatePin(myATM.Pin);
+        }
+        else if (myATM.Number.startsWith("2")) {
+            myATM.Type = "2";
+            PIN_6digits myPin = new PIN_6digits();
+            myATM.Pin = myPin.AskForPin();
+            valid = myPin.ValidatePin(myATM.Pin);
         }
 
-        String myPin= myAtm.askForPin();
-
-        myAtm.validatePin(myPin);
-
-
-    }
-
-    private void validatePin(String myPin) {
-        System.out.println("Thanks for typing a valid pin");
-        System.out.println(myPin);
-    }
-
-    private String askForPin() {
-        String PIN = null;
-
-        Scanner scanner = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-
-        if (myCard != null){
-
-            System.out.println("Please Select Card Type");
-            System.out.println("-------------------------------");
-            System.out.println("Type 1 (4 Dígitos Positivos)");
-            System.out.println("Type 2 (6 Dígitos Positivos)");
-            System.out.println("Type 3 (8 Chars Alfanumerícos)");
-            System.out.println(">> ");
-            int opc = scanner.nextInt();
-
-            System.out.println("Please Type Pin");
-            System.out.println(">> ");
-            PIN = scanner2.nextLine();
-
-            switch (opc){
-                case 1:
-                    if (PIN.matches("^(\\d\\d\\d\\d)$"))
-                        System.out.println("PIN Correcto: " + PIN);
-                    else
-                        System.out.println("ERROR: PIN Incorrecto: " + PIN);
-                break;
-
-                case 2:
-                    if (PIN.matches("^(\\d\\d\\d\\d\\d\\d)$"))
-                        System.out.println("PIN Correcto: " + PIN);
-                    else
-                        System.out.println("ERROR: PIN Incorrecto: " + PIN);
-                    break;
-
-                case 3:
-                    if (PIN.matches("^(\\w\\w\\w\\w\\w\\w\\w\\w)$"))
-                        System.out.println("PIN Correcto: " + PIN);
-                    else
-                        System.out.println("ERROR: PIN Incorrecto: " + PIN);
-                    break;
-                default:
-                    System.out.println("ERROR: Opción Incorrecta");
-            }
-
-        }
-        return PIN;
-
-    }
-
-    private void askForCard() throws IOException {
-
-        Scanner scanner = new Scanner(System.in);
-
-        if (myCard == null){
-            System.out.println("Please Insert Card");
-            System.out.println(">> ");
-            int CardNumber = scanner.nextInt();
-            if (CardNumber >= 1){
-                myCard = new MyBankCard();
-            }
-
-
+        else if (myATM.Number.startsWith("3")) {
+            myATM.Type = "3";
+            PIN_8alphanum myPin = new PIN_8alphanum();
+            myATM.Pin = myPin.AskForPin();
+            valid = myPin.ValidatePin(myATM.Pin);
         }
 
+        if (valid)
+            System.out.println("Thanks for typing a valid pin");
+        else
+            System.out.println("Sorry, that is an invalid pin");
+
     }
+
 
 
 }
