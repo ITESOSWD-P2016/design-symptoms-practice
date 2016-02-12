2d1574b9f2455d6d422f9e8d00103921e1537724
Ari <Ari>
2016-02-12 02:42:06 -0600

    Funcionalidad para imprimir recibos

---
+++ src/main/java/com/iteso/design/Cuentahabiente.java
@@ -0,0 +1,18 @@
+package com.iteso.design;
+
+public class Cuentahabiente 
+{
+	public String idCuentahabiente;
+	public String Nombre;
+	public String noCuenta;
+	public String nipActual;
+	public String nipAnterior;
+	public boolean nipActivo;
+	
+	public Cuentahabiente(String idCuentahabiente, String nombre, String noCuenta)
+	{
+		this.idCuentahabiente = idCuentahabiente;
+		this.Nombre = nombre;
+		this.noCuenta = noCuenta;
+	}
+}
