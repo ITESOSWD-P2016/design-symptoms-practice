0c3a5a77b293baa6519d7841a66d69d605b5ad4e
Rodrigo Villalobos <rvillalobos@tacitknowledge.com>
2016-02-15 00:08:34 -0600

    Arreglar merge

--- src/main/java/com/iteso/design/Cuentahabiente.java
+++ src/main/java/com/iteso/design/Cuentahabiente.java
@@ -1,18 +1,8 @@
 package com.iteso.design;
 
-public class Cuentahabiente 
-{
-	public String idCuentahabiente;
-	public String Nombre;
-	public String noCuenta;
-	public String nipActual;
-	public String nipAnterior;
-	public boolean nipActivo;
-	
-	public Cuentahabiente(String idCuentahabiente, String nombre, String noCuenta)
-	{
-		this.idCuentahabiente = idCuentahabiente;
-		this.Nombre = nombre;
-		this.noCuenta = noCuenta;
-	}
+/**
+ * Created by Sabino on 09/02/2016.
+ */
+public class CuentaHabiente {
+    
 }
